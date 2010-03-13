ed1b07e74579e9a325d60ddde63ac181e72f012b
Will Dietz <w@wdtz.org>
2010-03-13 17:26:48 -0600

    support both orientations, fix warnings

--- hw/kdrive/sdl/esFunc.h
+++ hw/kdrive/sdl/esFunc.h
@@ -20,10 +20,48 @@
 #define PI 3.1415926535897932384626433832795f
 #define ES_PI PI
 
+
+
 typedef struct
 {
     GLfloat m[4][4];
 } ESMatrix;
+
+/*-----------------------------------------------------------------------------
+ *  Method declarations
+ *-----------------------------------------------------------------------------*/
+
+GLuint esLoadShader ( GLenum type, const char *shaderSrc );
+
+GLuint esLoadProgram ( const char *vertShaderSrc, const char *fragShaderSrc );
+
+void
+esScale(ESMatrix *result, GLfloat sx, GLfloat sy, GLfloat sz);
+
+void
+esTranslate(ESMatrix *result, GLfloat tx, GLfloat ty, GLfloat tz);
+
+void
+esMatrixMultiply(ESMatrix *result, ESMatrix *srcA, ESMatrix *srcB);
+
+void
+esMatrixLoadIdentity(ESMatrix *result);
+
+void 
+esRotate(ESMatrix *result, GLfloat angle, GLfloat x, GLfloat y, GLfloat z);
+
+void 
+esFrustum(ESMatrix *result, float left, float right, float bottom, float top, float nearZ, float farZ);
+
+void
+esPerspective(ESMatrix *result, float fovy, float aspect, float nearZ, float farZ);
+
+void
+esOrtho(ESMatrix *result, float left, float right, float bottom, float top, float nearZ, float farZ);
+
+/*-----------------------------------------------------------------------------
+ *  Method definitions
+ *-----------------------------------------------------------------------------*/
 
 //
 ///
--- hw/kdrive/sdl/sdl.c
+++ hw/kdrive/sdl/sdl.c
@@ -63,9 +63,11 @@ static void checkError()
 #define checkError()
 #endif
 
-void GL_Init();
-void GL_InitTexture();
-void GL_Render();
+struct SdlGLESDriver;
+
+void GL_Init(void);
+void GL_InitTexture( struct SdlGLESDriver * driver );
+void GL_Render( struct SdlGLESDriver * driver );
 
 /*-----------------------------------------------------------------------------
  *  GL variables
@@ -113,7 +115,7 @@ float portrait_vertexCoords[] =
 
 //Pick an orientation
 //Have to update few other places to reflect changing this, fwiw
-float * vertexCoords = land_r_vertexCoords;
+float * vertexCoords = NULL;
 
 
 float texCoords[] =
@@ -126,20 +128,27 @@ float texCoords[] =
 
 GLushort indices[] = { 0, 1, 2, 1, 2, 3 };
 
+//We're using 24 bitdepth, each color has it's own byte
+//Note that the screen still is 32bit, but we use 24bit so we can upload
+//as a texture more easily (no reason to introduce alpha)
+int redMask = 0x0000ff;
+int greenMask = 0x00ff00;
+int blueMask = 0xff0000;
+
 static void xsdlFini(void);
 static Bool sdlScreenInit(KdScreenInfo *screen);
 static Bool sdlFinishInitScreen(ScreenPtr pScreen);
 static Bool sdlCreateRes(ScreenPtr pScreen);
 
 static void sdlKeyboardFini(KdKeyboardInfo *ki);
-static Bool sdlKeyboardInit(KdKeyboardInfo *ki);
-static Bool sdlKeyboardEnable(KdPointerInfo *pi);
-static Bool sdlKeyboardDisable(KdPointerInfo *pi);
+static Status sdlKeyboardInit(KdKeyboardInfo *ki);
+static Status sdlKeyboardEnable(KdKeyboardInfo *pi);
+static void sdlKeyboardDisable(KdKeyboardInfo *pi);
 
-static Bool sdlMouseInit(KdPointerInfo *pi);
+static Status sdlMouseInit(KdPointerInfo *pi);
 static void sdlMouseFini(KdPointerInfo *pi);
-static Bool sdlMouseEnable(KdPointerInfo *pi);
-static Bool sdlMouseDisable(KdPointerInfo *pi);
+static Status sdlMouseEnable(KdPointerInfo *pi);
+static void sdlMouseDisable(KdPointerInfo *pi);
 
 void *sdlShadowWindow (ScreenPtr pScreen, CARD32 row, CARD32 offset, int mode, CARD32 *size, void *closure);
 void sdlShadowUpdate (ScreenPtr pScreen, shadowBufPtr pBuf);
@@ -177,9 +186,8 @@ int mouseState=0;
 struct SdlGLESDriver
 {
     char * buffer;
-    //For now, I've hardcoded these
-    //int width;
-    //int height;
+    int width;
+    int height;
 };
 
 
@@ -187,14 +195,36 @@ struct SdlGLESDriver
 static Bool sdlScreenInit(KdScreenInfo *screen)
 {
 	struct SdlGLESDriver *sdlGLESDriver=calloc(1, sizeof(struct SdlGLESDriver));
+    SDL_Surface * s = NULL;
+
 #ifdef DEBUG
 	printf("sdlScreenInit()\n");
 #endif
+    //Set resolution if not specified
 	if (!screen->width || !screen->height)
 	{
-		screen->width = 320;
-		screen->height = 480;
+		screen->width = WIDTH;
+		screen->height = HEIGHT;
 	}
+
+    //Check that this is a valid resolution and determine orientation
+    if ( screen->width == WIDTH && screen->height == HEIGHT )
+    {
+        //Portrait orientation
+        vertexCoords = portrait_vertexCoords;
+    }
+    else if ( screen->height == WIDTH && screen->width == HEIGHT )
+    {
+        //Keyboard on the right
+        vertexCoords = land_r_vertexCoords;
+    }
+    else
+    {
+        //If this doesn't match either orientation, exit.
+        //We don't support scaling/panning presently.
+        return FALSE;
+    }
+
 	if (!screen->fb[0].depth)
 		screen->fb[0].depth = 32;
 #ifdef DEBUG
@@ -204,7 +234,7 @@ static Bool sdlScreenInit(KdScreenInfo *screen)
     {
         return FALSE;
     }
-	SDL_Surface * s = SDL_SetVideoMode(screen->width, screen->height, screen->fb[0].depth,
+	s = SDL_SetVideoMode( WIDTH, HEIGHT, screen->fb[0].depth,
             SDL_OPENGLES | SDL_FULLSCREEN );
     fprintf( stderr, "SetVideoMode: %p\n", s );
 	if( s == NULL )
@@ -212,20 +242,12 @@ static Bool sdlScreenInit(KdScreenInfo *screen)
 #ifdef DEBUG
 	printf("Set %dx%d/%dbpp mode\n", s->w, s->h, s->format->BitsPerPixel);
 #endif
-    //XXX: Bail if we DONT get the expected (hard-coded) resolution.
-
-    //We're using 24 bitdepth, each color has it's own byte
-    //Note that the screen still is 32bit, but we use 24bit so we can upload
-    //as a texture more easily (no reason to introduce alpha)
-    int redMask = 0x0000ff;
-    int greenMask = 0x00ff00;
-    int blueMask = 0xff0000;
 
     //Create buffer for rendering into
     sdlGLESDriver->buffer = malloc( WIDTH*HEIGHT*24 / 8 );
-    //Rotate
-    screen->width=s->h;
-	screen->height=s->w;
+    sdlGLESDriver->width = screen->width;
+    sdlGLESDriver->height = screen->height;
+
 	screen->fb[0].depth= 24;
 	screen->fb[0].visuals=(1<<TrueColor);
 	screen->fb[0].redMask=redMask;
@@ -237,13 +259,13 @@ static Bool sdlScreenInit(KdScreenInfo *screen)
 	screen->memory_size=0;
 	screen->off_screen_base=0;
 	screen->driver=sdlGLESDriver;
-	screen->fb[0].byteStride=(HEIGHT*24)/8;
-	screen->fb[0].pixelStride=HEIGHT;
+	screen->fb[0].byteStride=(screen->width*24)/8;
+	screen->fb[0].pixelStride=screen->width;
 	screen->fb[0].frameBuffer=(CARD8 *)sdlGLESDriver->buffer;
 	SDL_WM_SetCaption("Freedesktop.org X server (SDLGLES)", NULL);
 
     GL_Init();
-    GL_InitTexture();
+    GL_InitTexture( sdlGLESDriver );
 
 	return TRUE;
 }
@@ -299,52 +321,50 @@ static Bool sdlFinishInitScreen(ScreenPtr pScreen)
 
 static void sdlKeyboardFini(KdKeyboardInfo *ki)
 {
-        sdlKeyboard = NULL;
+    sdlKeyboard = NULL;
 }
 
-static Bool sdlKeyboardInit(KdKeyboardInfo *ki)
+static Status sdlKeyboardInit(KdKeyboardInfo *ki)
 {
-        ki->minScanCode = 8;
-        ki->maxScanCode = 255;
+    ki->minScanCode = 8;
+    ki->maxScanCode = 255;
 
-	sdlKeyboard = ki;
+    sdlKeyboard = ki;
 
-        return Success;
+    return Success;
 }
 
-static Bool sdlKeyboardEnable(KdPointerInfo *pi)
+static Status sdlKeyboardEnable(KdKeyboardInfo *pi)
 {
     //XXX: Is something supposed to happen here?
     return Success;
 }
 
-static Bool sdlKeyboardDisable(KdPointerInfo *pi)
+static void sdlKeyboardDisable(KdKeyboardInfo *pi)
 {
     //XXX: Is something supposed to happen here?
-    return Success;
 }
 
-static Bool sdlMouseInit (KdPointerInfo *pi)
+static Status sdlMouseInit (KdPointerInfo *pi)
 {
-        sdlPointer = pi;
-	return Success;
+    sdlPointer = pi;
+    return Success;
 }
 
 static void sdlMouseFini(KdPointerInfo *pi)
 {
-        sdlPointer = NULL;
+    sdlPointer = NULL;
 }
 
-static Bool sdlMouseEnable (KdPointerInfo *pi)
+static Status sdlMouseEnable (KdPointerInfo *pi)
 {
     //XXX: Is something supposed to happen here?
     return Success;
 }
 
-static Bool sdlMouseDisable (KdPointerInfo *pi)
+static void sdlMouseDisable (KdPointerInfo *pi)
 {
     //XXX: Is something supposed to happen here?
-    return Success;
 }
 
 void InitCard(char *name)
@@ -400,8 +420,23 @@ void sdlTimer(void)
 	while ( SDL_PollEvent(&event) ) {
 		switch (event.type) {
 			case SDL_MOUSEMOTION:
-                newx = event.motion.y;
-                newy = WIDTH - event.motion.x;
+                //Interpret mouse coordinates differently
+                //depending on the screen orientation
+                if ( vertexCoords == portrait_vertexCoords )
+                {
+                    newx = event.motion.x;
+                    newy = event.motion.y;
+                }
+                else if ( vertexCoords == land_r_vertexCoords )
+                {
+                    newx = event.motion.y;
+                    newy = WIDTH - event.motion.x;
+                }
+                else
+                {
+                    //Unexpected orientation
+                    break;
+                }
 				KdEnqueuePointerEvent(sdlPointer, mouseState, newx, newy, 0);
 				break;
 			case SDL_MOUSEBUTTONDOWN:
@@ -486,8 +521,27 @@ void OsVendorInit (void)
 /*-----------------------------------------------------------------------------
  *  GL stuff
  *-----------------------------------------------------------------------------*/
-void GL_Init()
+void GL_Init(void)
 {
+    static GLbyte vShaderStr[] =  
+        "attribute vec4 a_position;   \n"
+        "attribute vec2 a_texCoord;   \n"
+        "varying vec2 v_texCoord;     \n"
+        "void main()                  \n"
+        "{                            \n"
+        "   gl_Position = a_position; \n"
+        "   v_texCoord = a_texCoord;  \n"
+        "}                            \n";
+
+    static GLbyte fShaderStr[] =  
+        "precision mediump float;                            \n"
+        "varying vec2 v_texCoord;                            \n"
+        "uniform sampler2D s_texture;                        \n"
+        "void main()                                         \n"
+        "{                                                   \n"
+        "  gl_FragColor = texture2D( s_texture, v_texCoord );\n"
+        "}                                                   \n";
+
     // setup 2D gl environment
     checkError();
     glClearColor( 0.0f, 0.0f, 0.0f, 1.0f );//black background
@@ -498,25 +552,6 @@ void GL_Init()
     checkError();
     glDisable(GL_CULL_FACE);
     checkError();
-
-    GLbyte vShaderStr[] =  
-        "attribute vec4 a_position;   \n"
-        "attribute vec2 a_texCoord;   \n"
-        "varying vec2 v_texCoord;     \n"
-        "void main()                  \n"
-        "{                            \n"
-        "   gl_Position = a_position; \n"
-        "   v_texCoord = a_texCoord;  \n"
-        "}                            \n";
-
-    GLbyte fShaderStr[] =  
-        "precision mediump float;                            \n"
-        "varying vec2 v_texCoord;                            \n"
-        "uniform sampler2D s_texture;                        \n"
-        "void main()                                         \n"
-        "{                                                   \n"
-        "  gl_FragColor = texture2D( s_texture, v_texCoord );\n"
-        "}                                                   \n";
 
     // Load the shaders and get a linked program object
     programObject = esLoadProgram ( ( char *)vShaderStr, (char *)fShaderStr );
@@ -533,8 +568,10 @@ void GL_Init()
     checkError();
 }
 
-void GL_InitTexture()
+void GL_InitTexture( struct SdlGLESDriver * driver )
 {
+    int num;
+
     //delete it if we already have one
     if ( texture )
     {
@@ -548,7 +585,6 @@ void GL_InitTexture()
     checkError();
     
     //sanity check
-    int num;
     glGetIntegerv( GL_TEXTURE_BINDING_2D, &num );
     assert( num == texture );
     glGetIntegerv( GL_ACTIVE_TEXTURE, &num );
@@ -565,8 +601,8 @@ void GL_InitTexture()
     glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE );
     checkError();
 
-    glTexImage2D( GL_TEXTURE_2D, 0, GL_RGB, HEIGHT, WIDTH, 0, GL_RGB,
-            GL_UNSIGNED_BYTE, NULL );
+    glTexImage2D( GL_TEXTURE_2D, 0, GL_RGB, driver->width, driver->height,
+            0, GL_RGB, GL_UNSIGNED_BYTE, NULL );
     checkError();
 }
 
@@ -595,7 +631,7 @@ void GL_Render( struct SdlGLESDriver * driver )
 
     //Upload buffer to texture
     glTexSubImage2D( GL_TEXTURE_2D,0,
-            0,0, HEIGHT, WIDTH,
+            0,0, driver->width, driver->height,
             GL_RGB,GL_UNSIGNED_BYTE,driver->buffer );
     checkError();
 
